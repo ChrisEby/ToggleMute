156429ef366bfa8c34386ee4ad06aed78d38cd4c
Chris Eby <ChrisEby@users.noreply.github.com>
2017-12-11 18:05:47 -0500

    Adding in the code for toggling mute on windows machines.

---
+++ ToggleMute/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ToggleMute
+{
+    class Program
+    {
+        private const int AppCommandVolumeMute = 0x80000;
+        private const int Message = 0x319;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        static void Main(string[] args)
+        {
+            var handle = Process.GetCurrentProcess().MainWindowHandle;
+            
+            Console.WriteLine("Toggling Mute");
+            SendMessageW(handle, Message, handle, (IntPtr)AppCommandVolumeMute);
+        }
+    }
+}
